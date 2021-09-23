29f47c3f38278b99840e2a057f26d0abfdd03314
Nicola <ni.kim92@gmail.com>
2021-09-23 17:24:07 +0200

    ...

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect(route('platform.login.auth'));
 });
+
+Route::get('/home', function () {
+    return redirect('/admin');
+});
